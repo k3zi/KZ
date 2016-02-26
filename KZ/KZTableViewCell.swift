0ced46120a46a70b5616309d3b35978eff0ba172
Kesi Maduka <me@kez.io>
2016-02-25 23:54:00 -0600

    remove default separator color

--- KZ/KZTableViewCell.swift
+++ KZ/KZTableViewCell.swift
@@ -21,10 +21,8 @@ public class KZTableViewCell: UITableViewCell, Reusable {
     override required public init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = UIColor.clearColor()
-        
-        topSeperator.backgroundColor = RGB(239)
+
         self.contentView.addSubview(topSeperator)
-        bottomSeperator.backgroundColor = RGB(239)
         self.contentView.addSubview(bottomSeperator)
         
         self.contentView.bounds.size.height = 99999
