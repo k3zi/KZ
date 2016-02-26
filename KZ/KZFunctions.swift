c5ffd0519ceacf8d360ba52dfdfcdf24f8764ed4
Kesi Maduka <me@kez.io>
2016-02-25 23:53:09 -0600

    fix spacing

--- KZ/KZFunctions.swift
+++ KZ/KZFunctions.swift
@@ -17,27 +17,27 @@ public func RGB(x: CGFloat, a: CGFloat = 1.0) -> UIColor {
 }
 
 public func HEX(str: String) -> UIColor {
-    let hex = str.stringByTrimmingCharactersInSet(NSCharacterSet.alphanumericCharacterSet().invertedSet)
-    var int = UInt32()
-    NSScanner(string: hex).scanHexInt(&int)
-    let r, g, b: UInt32
-    switch hex.characters.count {
-    case 3: // RGB (12-bit)
-        (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-    case 6: // RGB (24-bit)
-        (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
-    default:
-        (r, g, b) = (1, 1, 0)
-    }
-    
-    return RGB(CGFloat(r), g: CGFloat(g), b: CGFloat(b))
+	let hex = str.stringByTrimmingCharactersInSet(NSCharacterSet.alphanumericCharacterSet().invertedSet)
+	var int = UInt32()
+	NSScanner(string: hex).scanHexInt(&int)
+	let r, g, b: UInt32
+	switch hex.characters.count {
+	case 3:
+		(r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+	case 6:
+		(r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+	default:
+		(r, g, b) = (1, 1, 0)
+	}
+
+	return RGB(CGFloat(r), g: CGFloat(g), b: CGFloat(b))
 }
 
-public func delay(delay:Double, closure:()->()) {
-    dispatch_after(
-        dispatch_time(
-            DISPATCH_TIME_NOW,
-            Int64(delay * Double(NSEC_PER_SEC))
-        ),
-        dispatch_get_main_queue(), closure)
+public func delay(delay: Double, closure: () -> ()) {
+	dispatch_after(
+		dispatch_time(
+			DISPATCH_TIME_NOW,
+			Int64(delay * Double(NSEC_PER_SEC))
+		),
+		dispatch_get_main_queue(), closure)
 }
